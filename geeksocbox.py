20b786b7f4a38fb2a7c87265926a8a48b453bf61
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-10-04 20:08:29 +0100

    added code to try and make the program just run and update files as it can, but it doesn't work

--- geeksocbox.py
+++ geeksocbox.py
@@ -6,6 +6,20 @@ import time
 from os import listdir
 from os.path import isfile, join
 
+dot = '.'
+files = []
+times = []
+filelist = open("filelist.txt",'r')
+timelist = open("timelist.txt",'r')
+for filename in filelist:
+	files.append(filename.rstrip())
+
+for filetime in timelist:
+	times.append(filetime.rstrip())
+
+filelist.close()
+timelist.close()
+
 #server = raw_input("Server: ")
 #port = int(raw_input("Port Number: "))
 server = 'shell.geeksoc.org'
@@ -20,15 +34,28 @@ ssh.connect(server, port, username, password)
 # SCPCLient takes a paramiko transport as its only argument
 scp = SCPClient(ssh.get_transport())
 
-onlyfiles = [ f for f in listdir('/Users/Iain/Documents/Programming/GeekSocBox/') if isfile(join('/Users/Iain/Documents/Programming/GeekSocBox/',f)) ]
+try:
+    while True:
+        for i in range(len(times)):
+        	if files[i][:1] != dot:
+        		if times[i] < os.stat(files[i]).st_mtime:
+        			scp.put(files[i])
+        			times[i] = os.stat(files[i]).st_mtime 
+        			print "update"
+except KeyboardInterrupt:
+    pass # do cleanup here
 
-f = open("filelist.txt",'w')
+files = [ f for f in listdir('/Users/Iain/Documents/Programming/GeekSocBox/') if isfile(join('/Users/Iain/Documents/Programming/GeekSocBox/',f)) ]
 
-print(onlyfiles)
-for item in onlyfiles:
-	f.write("%s\n" % item)
+filelist = open("filelist.txt",'w')
+timelist = open("timelist.txt",'w')
 
-var = os.stat('test2.txt').st_mtime
-var2 = os.stat('test2.txt').st_mtime
+for i in range(len(files)):
+	filelist.write("%s\n" % files[i].rstrip())
+	timelist.write("%s\n" % times[i].rstrip())
+
+
+filelist.close()
+timelist.close()
 
 scp.put('test2.txt')
