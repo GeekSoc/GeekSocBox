8dcdbf655c8687f36fed6b30fd7b3152540e89c7
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-10-04 18:06:24 +0100

    added user inputs

--- geeksocbox.py
+++ geeksocbox.py
@@ -1,12 +1,17 @@
 from paramiko import SSHClient
 from scp import SCPClient
+import getpass
+
+server = raw_input("Server: ")
+port = int(raw_input("Port Number: "))
+username = raw_input("Username: ")
+password = getpass.getpass("Password: ")
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
-ssh.connect('shell.geeksoc.org', 22, 'icuthbertson', 'Dexter0502')
+ssh.connect(server, port, username, password)
 
 # SCPCLient takes a paramiko transport as its only argument
 scp = SCPClient(ssh.get_transport())
 
-scp.put('test.txt', 'test2.txt')
-scp.get('test2.txt')
+scp.put('test2.txt')
