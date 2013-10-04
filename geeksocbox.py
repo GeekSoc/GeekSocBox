7d9e16231f367ae75cf81e173dbdcf5997bc1d2b
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-10-04 19:04:55 +0100

    added code to get all files in the folder and save the list to a file

--- geeksocbox.py
+++ geeksocbox.py
@@ -3,6 +3,8 @@ from scp import SCPClient
 import getpass
 import os
 import time
+from os import listdir
+from os.path import isfile, join
 
 #server = raw_input("Server: ")
 #port = int(raw_input("Port Number: "))
@@ -18,13 +20,15 @@ ssh.connect(server, port, username, password)
 # SCPCLient takes a paramiko transport as its only argument
 scp = SCPClient(ssh.get_transport())
 
-#scp.put('test2.txt')
+onlyfiles = [ f for f in listdir('/Users/Iain/Documents/Programming/GeekSocBox/') if isfile(join('/Users/Iain/Documents/Programming/GeekSocBox/',f)) ]
+
+f = open("filelist.txt",'w')
+
+print(onlyfiles)
+for item in onlyfiles:
+	f.write("%s\n" % item)
 
 var = os.stat('test2.txt').st_mtime
 var2 = os.stat('test2.txt').st_mtime
-
-while var == var2:
-	var2 = os.stat('test2.txt').st_mtime
-	time.sleep(10)
 
 scp.put('test2.txt')
