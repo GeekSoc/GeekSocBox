b9d65e10d6ccd63538af0003c691729973ea757d
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-10-04 18:33:20 +0100

    added code that uploads a new version of the file if the file is modified

--- geeksocbox.py
+++ geeksocbox.py
@@ -1,9 +1,13 @@
 from paramiko import SSHClient
 from scp import SCPClient
 import getpass
+import os
+import time
 
-server = raw_input("Server: ")
-port = int(raw_input("Port Number: "))
+#server = raw_input("Server: ")
+#port = int(raw_input("Port Number: "))
+server = 'shell.geeksoc.org'
+port = int(22)
 username = raw_input("Username: ")
 password = getpass.getpass("Password: ")
 
@@ -13,5 +17,14 @@ ssh.connect(server, port, username, password)
 
 # SCPCLient takes a paramiko transport as its only argument
 scp = SCPClient(ssh.get_transport())
+
+#scp.put('test2.txt')
+
+var = os.stat('test2.txt').st_mtime
+var2 = os.stat('test2.txt').st_mtime
+
+while var == var2:
+	var2 = os.stat('test2.txt').st_mtime
+	time.sleep(10)
 
 scp.put('test2.txt')
